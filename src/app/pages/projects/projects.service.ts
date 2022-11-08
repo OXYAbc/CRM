69e3e9d6d076944635f2827dd1d4ee9c443fc188
OXYAbc <100861294+OXYAbc@users.noreply.github.com>
2022-11-08 13:33:55 +0100

    new seach in projects

--- src/app/pages/projects/projects.service.ts
+++ src/app/pages/projects/projects.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Project, Task } from 'src/app/models/projects.model';
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable, tap } from 'rxjs';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
@@ -8,36 +8,46 @@ import {
 
 @Injectable({ providedIn: 'root' })
 export class ProjectsService {
-  private projectCollection: AngularFirestoreCollection<Project>;
-  public project$: Observable<Project[]>;
-
-  constructor(public angularFirestore: AngularFirestore) {
-    this.projectCollection = this.angularFirestore.collection('Projects', (ref) => ref);
-
-    this.project$ = this.projectCollection
-      .snapshotChanges()
-      .pipe(
-        map((changes) =>
-          changes.map((change) => {
-            const task = new Project({ ...change.payload.doc.data(), id: change.payload.doc.id })
-            return task
-          })
-        )
-      );
-  }
+  public searchWord = new BehaviorSubject<string>('');
+  projectCollection: AngularFirestoreCollection<Project> =
+    this.angularFirestore.collection('Projects', (ref) => ref);
+  project$: Observable<Project[]> = this.projectCollection
+    .snapshotChanges()
+    .pipe(
+      map((changes) =>
+        changes.map((change) => {
+          const task = new Project({
+            ...change.payload.doc.data(),
+            id: change.payload.doc.id,
+          });
+          return task;
+        })
+      )
+    );
+  public projects$ = combineLatest([this.project$, this.searchWord]).pipe(
+    map(([project, searchWord]) => {
+      return project.filter((item) => {
+        return item.name
+          .toLocaleLowerCase()
+          .includes(searchWord.toLocaleLowerCase());
+      });
+    })
+  );
+  constructor(public angularFirestore: AngularFirestore) {}
 
   addProject(project: Project) {
     this.projectCollection.add(project);
   }
 
+  setSearchWord(word: string) {
+    this.searchWord.next(word);
+  }
+
   getProject(id: string) {
     return this.angularFirestore.collection('Projects').doc(id).valueChanges();
   }
-  updateTasks(tasksArray:Task[], id:string){
-    this.angularFirestore
-    .collection('Projects')
-    .doc(id)
-    .update({
+  updateTasks(tasksArray: Task[], id: string) {
+    this.angularFirestore.collection('Projects').doc(id).update({
       tasks: tasksArray,
     });
   }
