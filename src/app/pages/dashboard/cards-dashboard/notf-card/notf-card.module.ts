45d1615033f7d1798dba4e6f8b94b8da85f87cea
OXYAbc <100861294+OXYAbc@users.noreply.github.com>
2022-11-16 10:46:15 +0100

    add share table in dashboard/notf

--- src/app/pages/dashboard/cards-dashboard/notf-card/notf-card.module.ts
+++ src/app/pages/dashboard/cards-dashboard/notf-card/notf-card.module.ts
@@ -2,10 +2,11 @@ import { CdkTableModule } from '@angular/cdk/table';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TableModule } from 'src/app/shared/components/table/table.module';
 import { NotfCardComponent } from './notf-card.component';
 
 @NgModule({
-  imports: [CdkTableModule, CommonModule, RouterModule],
+  imports: [CdkTableModule, CommonModule, RouterModule, TableModule],
   declarations: [NotfCardComponent],
   exports: [NotfCardComponent],
   providers: [],
