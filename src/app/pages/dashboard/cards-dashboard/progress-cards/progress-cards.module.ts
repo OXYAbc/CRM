f03191d90435fbfeb8aa74a54adab8c186e0be7b
OXYAbc <100861294+OXYAbc@users.noreply.github.com>
2022-11-18 09:44:43 +0100

    new directiv for title attr. and stage-box

--- src/app/pages/dashboard/cards-dashboard/progress-cards/progress-cards.module.ts
+++ src/app/pages/dashboard/cards-dashboard/progress-cards/progress-cards.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CheckStage } from 'src/app/@theme/app-check-stage.directive';
+import { TitleProgress } from 'src/app/@theme/app-title-progress.directive';
 import { ProgressCardsComponent } from './progress-cards.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [ProgressCardsComponent],
+  declarations: [ProgressCardsComponent, CheckStage, TitleProgress],
   exports: [ProgressCardsComponent],
   providers: [],
 })
