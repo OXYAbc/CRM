2f90ad38815471b5fb5a6a41d22db4e829530af6
OXYAbc <100861294+OXYAbc@users.noreply.github.com>
2022-11-22 09:33:08 +0100

    Fixed a bug that prevented the application from compiling

--- src/app/pages/dashboard/cards-dashboard/progress-cards/progress-cards.module.ts
+++ src/app/pages/dashboard/cards-dashboard/progress-cards/progress-cards.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CheckStage } from 'src/app/@theme/app-check-stage.directive';
 import { TitleProgress } from 'src/app/@theme/app-title-progress.directive';
-import { ProgressCardsComponent } from './progress-cards.component';
+import { ProgressCardComponent } from './progress-cards.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [ProgressCardsComponent, CheckStage, TitleProgress],
-  exports: [ProgressCardsComponent],
+  declarations: [ProgressCardComponent, CheckStage, TitleProgress],
+  exports: [ProgressCardComponent],
   providers: [],
 })
 export class ProgressCardsModule {}
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,12 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ProjectsService } from '../projects/projects.service';
+import { TasksService } from '../tasks/tasks.service';
 import { CardsDashboardModule } from './cards-dashboard/cards-dashboard.module';
 import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [CardsDashboardModule, HttpClientModule],
   declarations: [DashboardComponent],
-  providers: [ProjectsService],
+  providers: [ProjectsService, TasksService],
 })
 export class DashboardModule {}
