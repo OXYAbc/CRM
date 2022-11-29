1cacaaec293ede9cdb1920d8bb8d0a58129ce14a
OXYAbc <100861294+OXYAbc@users.noreply.github.com>
2022-11-29 15:39:26 +0100

    adding user name to postedt comment

--- src/app/pages/tasks/cards-tasks/detail-view/comments/comments.component.ts
+++ src/app/pages/tasks/cards-tasks/detail-view/comments/comments.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, EventEmitter, Inject } from '@angular/core';
 import { DIALOG_DATA } from '@angular/cdk/dialog';
 import { TasksService } from '../../../tasks.service';
 import { Comment } from 'src/app/models/tasks.model';
@@ -9,14 +9,19 @@ import { Comment } from 'src/app/models/tasks.model';
   styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent {
+  onsave = new EventEmitter<string>()
   constructor(
     @Inject(DIALOG_DATA)
     public data: {
       comments: { user: string; comment: string }[];
       itemID: string;
+      userName: string
     },
     private taskService: TasksService
   ) {
     data.comments = data.comments.filter((res) => res.user != null);
+  }
+  onSave(form :any){
+    this.onsave.emit(form)
   }
 }
